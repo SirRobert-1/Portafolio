e79126fbaeebc829fe16662953ee2bf1cae5f173
SirRobert-1 <rviverosgonzalez@gmail.com>
2021-11-18 16:48:04 -0600

    Url actualizados

--- components/Hero.vue
+++ components/Hero.vue
@@ -4,7 +4,11 @@
       <p class="hero__subtitle">I'm Product Designer</p>
       <p class="hero__title">Hello, my name is Roberto.</p>
       <div>
-        <img class="h-10 w-10" :src="face" alt="" />
+        <img
+          class="h-10 w-10"
+          src="@/assets/images/icons/facebook.svg"
+          alt=""
+        />
       </div>
     </div>
   </section>
